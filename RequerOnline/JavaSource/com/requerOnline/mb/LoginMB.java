bfd034a73cc960f34f6436dbc20b457286a37517
fabioflb85 <fabioflb85@gmail.com>
2015-01-01 21:27:01 +0000

    Implementação da lógica de login. Falta implementar a parte visual. Atentar para a melhor estratégia do método localizar

--- RequerOnline/JavaSource/com/requerOnline/mb/LoginMB.java
+++ RequerOnline/JavaSource/com/requerOnline/mb/LoginMB.java
@@ -1,48 +1,31 @@
 package com.requerOnline.mb;
 
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
 import com.requerOnline.applicationContext.SpringBeans;
-import com.requerOnline.core.Gerenciador;
 import com.requerOnline.core.GerenciadorConectado;
-import com.requerOnline.core.Secretario;
 import com.requerOnline.service.ServiceDelegate;
 import com.requerOnline.serviceImpl.ServiceDelegateImpl;
-import com.requerOnline.util.enums.PerfilTypes;
 
 @ManagedBean
 @SessionScoped
 public class LoginMB {
-	Secretario s = new Secretario();
-	private GerenciadorConectado gerenciadorConectado = new GerenciadorConectado();
+	private GerenciadorConectado gerenciadorConectado;
 	private Boolean logou;
 	private ServiceDelegate delegate;
 	
-	public LoginMB(){
-		gerenciadorConectado.setGerenciador(s);
+	@PostConstruct
+	public void iniciar(){
+		gerenciadorConectado = SpringBeans.getBeanGerenciadorConectado();
 	}
-
-	// De acordo com o perfil selecionado na tela, a instância de gerenciador
-	// será determinada
-	public void instanciarTipoGerenciador() {
-		if (PerfilTypes.ADMINISTRADOR.equals(gerenciadorConectado
-				.getGerenciador().getPerfil())) {
-			gerenciadorConectado.setGerenciador(SpringBeans
-					.getBeanAdministrador());
-		} else if (PerfilTypes.SECRETARIO.equals(gerenciadorConectado
-				.getGerenciador().getPerfil())) {
-			gerenciadorConectado
-					.setGerenciador(SpringBeans.getBeanSecretario());
-		}
-	}
-
+	
 	public void logar() {
-		instanciarTipoGerenciador();
 		delegate = new ServiceDelegateImpl();
 		gerenciadorConectado = delegate.getLoginService().getGerenciadorLogado(
 				gerenciadorConectado.getGerenciador());
-		if(gerenciadorConectado.getDataHoraLogin() != null){
+		if(gerenciadorConectado != null){
 			System.out.println("logou");
 		}else{
 			System.out.println("deu bronca");
