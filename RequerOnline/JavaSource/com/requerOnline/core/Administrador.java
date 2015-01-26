52dca026e85a472c986c27dfaf5a4c6c07cba778
fabioflb85 <fabioflb85@gmail.com>
2015-01-26 02:02:38 +0000

    Tela cadastrar requerente. necessário ver não funcionamento do JQuery para máscaras

--- RequerOnline/JavaSource/com/requerOnline/core/Administrador.java
+++ RequerOnline/JavaSource/com/requerOnline/core/Administrador.java
@@ -1,27 +1,15 @@
 package com.requerOnline.core;
 
 import javax.persistence.Entity;
-import javax.validation.constraints.NotNull;
 
 import org.springframework.stereotype.Service;
 
 @Service
 @Entity
 public class Administrador extends Gerenciador{
-
-	@NotNull(message="Insira um setor.")
-	private String setor;
 	
 	public Administrador() {
 		// TODO Auto-generated constructor stub
-	}
-
-	public String getSetor() {
-		return setor;
-	}
-
-	public void setSetor(String setor) {
-		this.setor = setor;
 	}
 
 }
--- RequerOnline/JavaSource/com/requerOnline/core/Secretario.java
+++ RequerOnline/JavaSource/com/requerOnline/core/Secretario.java
@@ -1,7 +1,6 @@
 package com.requerOnline.core;
 
 import javax.persistence.Entity;
-import javax.validation.constraints.NotNull;
 
 import org.springframework.stereotype.Service;
 
@@ -9,19 +8,8 @@ import org.springframework.stereotype.Service;
 @Entity
 public class Secretario extends Gerenciador{
 
-	@NotNull(message="Insira uma secretaria.")
-	private String secretaria;
-	
 	public Secretario() {
 		// TODO Auto-generated constructor stub
-	}
-
-	public String getSecretaria() {
-		return secretaria;
-	}
-
-	public void setSecretaria(String secretaria) {
-		this.secretaria = secretaria;
 	}
 
 }
