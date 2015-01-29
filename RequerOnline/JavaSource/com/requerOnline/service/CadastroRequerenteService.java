45b802feb16fd858f63e06ec3399f20d1c9dc076
fabioflb85 <fabioflb85@gmail.com>
2015-01-29 02:45:57 +0000

    Início da implementação do fluxo de salvar requerente

---
+++ RequerOnline/JavaSource/com/requerOnline/service/CadastroRequerenteService.java
@@ -0,0 +1,7 @@
+package com.requerOnline.service;
+
+import com.requerOnline.core.Requerente;
+
+public interface CadastroRequerenteService {
+	public void salvar(Requerente requerente);
+}
--- RequerOnline/JavaSource/com/requerOnline/serviceImpl/ServiceDelegateImpl.java
+++ RequerOnline/JavaSource/com/requerOnline/serviceImpl/ServiceDelegateImpl.java
@@ -1,5 +1,6 @@
 package com.requerOnline.serviceImpl;
 
+import com.requerOnline.service.CadastroRequerenteService;
 import com.requerOnline.service.LoginService;
 import com.requerOnline.service.ServiceDelegate;
 
@@ -8,6 +9,11 @@ public class ServiceDelegateImpl implements ServiceDelegate{
 	@Override
 	public LoginService getLoginService() {
 		return (LoginService) new LoginServiceImpl();
+	}
+
+	@Override
+	public CadastroRequerenteService getCadastroRequerenteService() {
+		return (CadastroRequerenteService) new CadastroRequerenteServiceImpl();
 	}	
 
 }
