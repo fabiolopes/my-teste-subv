41136bc9b76350e839a5840d55119253ba732c8f
fabioflb85@gmail.com <fabioflb85@gmail.com@2c1f9f8e-b6ff-b70e-771b-38b6ec5201fc>
2015-02-02 02:22:41 +0000

    Modal para apresentar mensagem de erro e sucesso requerente. Tratar quando houver erro de validação.

--- trunk/RequerOnline/JavaSource/com/requerOnline/core/Funcionario.java
+++ trunk/RequerOnline/JavaSource/com/requerOnline/core/Funcionario.java
@@ -3,14 +3,18 @@ package com.requerOnline.core;
 import java.io.Serializable;
 
 import javax.persistence.Column;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
+import javax.validation.constraints.Pattern;
 
 import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotEmpty;
 import org.hibernate.validator.constraints.br.CPF;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -32,6 +36,34 @@ public abstract class Funcionario implements Serializable{
 
 	@Email(message="Insira um email válido.")
 	private String email;
+	
+	@NotEmpty(message = "Insira uma nacionalidade.")
+	private String nacionalidade;
+
+	@NotEmpty
+	@Length(min = 5, message = "Insira um RG válido.")
+	private String rg;
+
+	@Autowired
+	@Embedded
+	private Endereco endereco;
+
+	@Pattern(regexp = "^\\(?\\d{2}\\)?[\\s-]?\\d{4}-?\\d{4}$", message="Insira um telefone válido")
+	@NotEmpty(message="Insira um telefone de contato.")
+	private String telefone;
+	
+	@NotEmpty(message="Informe o vínculo empregatício.")
+	private String vinculo;
+	
+	@NotEmpty(message="Informe a função.")
+	private String funcao;
+	
+	@NotEmpty(message="Informe a secretaria de lotação.")
+	private String secretaria;
+	
+	@NotEmpty(message="Informe o setor de origem.")
+	private String setor;
+
 
 	public Funcionario() {
 		// TODO Auto-generated constructor stub
@@ -69,4 +101,68 @@ public abstract class Funcionario implements Serializable{
 		this.email = email;
 	}
 
+	public String getNacionalidade() {
+		return nacionalidade;
+	}
+
+	public void setNacionalidade(String nacionalidade) {
+		this.nacionalidade = nacionalidade;
+	}
+
+	public String getRg() {
+		return rg;
+	}
+
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+
+	public Endereco getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getVinculo() {
+		return vinculo;
+	}
+
+	public void setVinculo(String vinculo) {
+		this.vinculo = vinculo;
+	}
+
+	public String getFuncao() {
+		return funcao;
+	}
+
+	public void setFuncao(String funcao) {
+		this.funcao = funcao;
+	}
+
+	public String getSecretaria() {
+		return secretaria;
+	}
+
+	public void setSecretaria(String secretaria) {
+		this.secretaria = secretaria;
+	}
+
+	public String getSetor() {
+		return setor;
+	}
+
+	public void setSetor(String setor) {
+		this.setor = setor;
+	}
+	
 }
--- trunk/RequerOnline/JavaSource/com/requerOnline/core/Requerente.java
+++ trunk/RequerOnline/JavaSource/com/requerOnline/core/Requerente.java
@@ -1,12 +1,7 @@
 package com.requerOnline.core;
 
-import javax.persistence.Embedded;
 import javax.persistence.Entity;
-import javax.validation.constraints.Pattern;
 
-import org.hibernate.validator.constraints.Length;
-import org.hibernate.validator.constraints.NotEmpty;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,100 +9,9 @@ import org.springframework.stereotype.Service;
 public class Requerente extends Funcionario{
 
 	private static final long serialVersionUID = -7554507026090905682L;
-
-	@NotEmpty(message = "Insira uma nacionalidade.")
-	private String nacionalidade;
-
-	@NotEmpty
-	@Length(min = 5, message = "Insira um RG válido.")
-	private String rg;
-
-	@Autowired
-	@Embedded
-	private Endereco endereco;
-
-	@Pattern(regexp = "^\\(?\\d{2}\\)?[\\s-]?\\d{4}-?\\d{4}$", message="Insira um telefone válido")
-	@NotEmpty(message="Insira um telefone de contato.")
-	private String telefone;
 	
-	@NotEmpty(message="Informe o vínculo empregatício.")
-	private String vinculo;
-	
-	@NotEmpty(message="Informe a função.")
-	private String funcao;
-	
-	@NotEmpty(message="Informe a secretaria de lotação.")
-	private String secretaria;
-	
-	@NotEmpty(message="Informe o setor de origem.")
-	private String setor;
-
 	public Requerente() {
 		// TODO Auto-generated constructor stub
-	}
-
-	public String getNacionalidade() {
-		return nacionalidade;
-	}
-
-	public void setNacionalidade(String nacionalidade) {
-		this.nacionalidade = nacionalidade;
-	}
-
-	public String getRg() {
-		return rg;
-	}
-
-	public void setRg(String rg) {
-		this.rg = rg;
-	}
-
-	public Endereco getEndereco() {
-		return endereco;
-	}
-
-	public void setEndereco(Endereco endereco) {
-		this.endereco = endereco;
-	}
-
-	public String getTelefone() {
-		return telefone;
-	}
-
-	public void setTelefone(String telefone) {
-		this.telefone = telefone;
-	}
-
-	public String getVinculo() {
-		return vinculo;
-	}
-
-	public void setVinculo(String vinculo) {
-		this.vinculo = vinculo;
-	}
-
-	public String getFuncao() {
-		return funcao;
-	}
-
-	public void setFuncao(String funcao) {
-		this.funcao = funcao;
-	}
-
-	public String getSecretaria() {
-		return secretaria;
-	}
-
-	public void setSecretaria(String secretaria) {
-		this.secretaria = secretaria;
-	}
-
-	public String getSetor() {
-		return setor;
-	}
-
-	public void setSetor(String setor) {
-		this.setor = setor;
 	}
 
 }
